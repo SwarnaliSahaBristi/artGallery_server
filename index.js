be0dd81f97b7bf00b5edc51989a970de173a47e4
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2025-11-11 06:52:37 +0600

    basic functions added

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster2002.1tfbne8.mongodb.net/?appName=Cluster2002`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Art GAllery server is starting!')
+})
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Art GAllery is listening on port ${port}`)
+})
