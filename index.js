fdc1f2d0a13cda5aba959df9dc2bc1ee57061b4b
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2025-11-12 18:58:39 +0600

    update method added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 3000
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
 app.use(cors());
@@ -17,53 +17,82 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', (req, res) => {
-  res.send('Art GAllery server is starting!')
-})
+app.get("/", (req, res) => {
+  res.send("Art GAllery server is starting!");
+});
 
 async function run() {
   try {
     await client.connect();
 
-    const db = client.db("artify-db")
-    const artCollection = db.collection("arts")
-    const favCollection = db.collection("favorites")
+    const db = client.db("artify-db");
+    const artCollection = db.collection("arts");
+    const favCollection = db.collection("favorites");
 
-    app.get('/arts', async(req,res)=>{
-        const result = await artCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/arts", async (req, res) => {
+      const result = await artCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.post('/arts', async(req,res)=>{
-        const data = req.body;
-        const result = await artCollection.insertOne(data);
-        res.send(result);
-    })
+    app.post("/arts", async (req, res) => {
+      const data = req.body;
+      const result = await artCollection.insertOne(data);
+      res.send(result);
+    });
 
-    app.get('/search', async(req,res)=>{
-        const search_text = req.query.search;
-        const result = await artCollection.find({title: {$regex: search_text, $options: "i"}}).toArray();
-        res.send(result);
-    })
+    app.get("/my-gallery", async (req, res) => {
+      const email = req.query.email;
+      const result = await artCollection.find({ userEmail: email }).toArray();
+      res.send(result);
+    });
 
-    app.get('/arts/:id', async(req,res)=>{
-        const {id} = req.params;
-        const objectId = new ObjectId(id);
-        const result = await artCollection.findOne({ _id: objectId });
-        res.send(result);
-    })
+    app.put("/arts/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedArtwork = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateArt = {
+        $set: {
+          title: updatedArtwork.title,
+          medium: updatedArtwork.medium,
+          category: updatedArtwork.category,
+          dimensions: updatedArtwork.dimensions,
+          price: updatedArtwork.price,
+          visibility: updatedArtwork.visibility,
+          description: updatedArtwork.description,
+          imageUrl: updatedArtwork.imageUrl,
+        },
+      };
+      const result = await artCollection.updateOne(filter, updateArt);
+      res.send(result);
+    });
+
+    app.get("/search", async (req, res) => {
+      const search_text = req.query.search;
+      const result = await artCollection
+        .find({ title: { $regex: search_text, $options: "i" } })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/arts/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const result = await artCollection.findOne({ _id: objectId });
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
   }
 }
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Art GAllery is listening on port ${port}`)
-})
+  console.log(`Art GAllery is listening on port ${port}`);
+});
